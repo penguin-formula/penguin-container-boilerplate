2fc9fe80614d48f4ad3d5568d97694a34d4efd88
Eduardo Thomaz de Carvalho <ethomaz@id.uff.br>
2018-11-14 10:53:06 +0000

    add swagger

--- src/penguin/penguin.dto.ts
+++ src/penguin/penguin.dto.ts
@@ -1,6 +1,16 @@
+import { ApiModelProperty } from '@nestjs/swagger'
+
 export class PenguinDto {
+
+    @ApiModelProperty()
     readonly name: string;
+
+    @ApiModelProperty()
     readonly age: number;
+
+    @ApiModelProperty()
     readonly color: string;
+
+    @ApiModelProperty()
     readonly specie: string;
 }
