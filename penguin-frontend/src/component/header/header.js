ded4a6cbe78b4f3e5366752ac23e58f258a47ceb
Martin <martinkrol89@gmail.com>
2018-11-18 23:38:32 +0000

    gróóória deush

---
+++ penguin-frontend/src/component/header/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="app-header">
+        <img src="http://www.penguinformula.com/wp-content/uploads/2018/09/penguin_l_web_200_neg_laranjs.png" />
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ penguin-frontend/src/component/penguinlist/penguinlist.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './penguinlist.css';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+class Penguinlist extends Component {
+  constructor(props) {
+    super(props);
+    this.penguins = [
+      {
+        name: 'Mad Max',
+        age: 5,
+        color: 'Yellow',
+        specie: 'Emperor',
+      },
+      {
+        name: 'Ice cream sandwich',
+        age: 237,
+        color: 'Yellow',
+        specie: 'Emperor',
+      },
+      { name: 'Eclair', age: 262, color: 'Yellow', specie: 'Emperor' },
+      { name: 'Cupcake', age: 305, color: 'Yellow', specie: 'Emperor' },
+      {
+        name: 'Gingerbread',
+        age: 356,
+        color: 'Yellow',
+        specie: 'Emperor',
+      },
+    ];
+  }
+
+  render() {
+    //return <div className="penguinlist">aa</div>;
+    return this.renderTable();
+  }
+
+  renderTable() {
+    return (
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell numeric>age</TableCell>
+              <TableCell>color</TableCell>
+              <TableCell>specie</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.penguins.map(row => {
+              return (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell numeric>{row.age}</TableCell>
+                  <TableCell>{row.color}</TableCell>
+                  <TableCell>{row.specie}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+}
+
+export default Penguinlist;
