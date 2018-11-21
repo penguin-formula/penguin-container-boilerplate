7a6074ccdb738d14c987ab27a182f921813bbbab
Eduardo Thomaz de Carvalho <ethomaz@id.uff.br>
2018-11-21 10:19:56 +0000

    kubernetes endpoints

--- penguin-api/src/app.module.ts
+++ penguin-api/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PenguinModule } from './penguin/penguin.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [PenguinModule],
+  controllers: [AppController],
+  providers: [
+    AppService,
+  ],
 })
 export class AppModule {}
